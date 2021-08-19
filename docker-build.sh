4ff1c0e0469cb53f02961a4725d21b50b013e975
Lewis Vail <lewisvail3@gmail.com>
2021-08-19 09:28:43 -0400

    Removing JAR unpacking from docker-build.sh
    The unpacked resources are not currently used.

--- docker-build.sh
+++ docker-build.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-./mvnw clean package || exit 1
-
-mkdir -p target/dependency
-pushd target/dependency
-jar -xf ../*.jar
-popd
-
-docker build -t metrics-manager .
+./mvnw clean package && docker build -t metrics-manager .
